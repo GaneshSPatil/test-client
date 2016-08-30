1911ad1bad0850dfb4b883d1d07714a67e41314b
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-08-30 09:00:33 +0530

    foop

--- client.js
+++ client.js
@@ -2,3 +2,14 @@ var request = require('request');
 request('http://localhost:3000', function (error, response, body) {
   console.log(response);
 });
+
+request.post({
+  url:'http://localhost:3000/submit',
+  form: {foo : "bar"}
+},
+  function optionalCallback(err, httpResponse, body) {
+  if (err) {
+    return console.error('upload failed:', err);
+  }
+  console.log('Upload successful!  Server responded with:', body);
+});
